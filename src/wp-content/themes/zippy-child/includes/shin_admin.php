9a6b1763c585f43cfee3cbddc01fa91e5c128275
shin <nhathanh024@gmail.com>
2024-06-21 14:39:19 +0700

    add code

--- src/wp-content/themes/zippy-child/includes/shin_admin.php
+++ src/wp-content/themes/zippy-child/includes/shin_admin.php
@@ -5,7 +5,7 @@
  */
 add_filter('admin_footer_text', 'shin_change_footer_text');
 function shin_change_footer_text() {
-    echo "Core developed by <span ><a href='https://theshin.online' target='_blank'>Shin</a> or call me <a href='tel:0966514360'>0966514360</a></span> ";
+    echo "Core developed by <span ><a href='https://zippy.sg' target='_blank'>Zippy</a></span> ";
 }
 
 /*
